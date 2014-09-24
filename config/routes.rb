68ee94cf392b4f36bc2e2add8108f2f5bd1ba66c
Din Altair <Dinaltair@gmail.com>
2014-09-24 14:33:25 +0000

    Add user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  resources :users
+  
+  get 'users/new'
+  
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',   to: 'users#new', via: 'get'
+  
   get 'static_pages/home'
   
   get 'static_pages/help'
